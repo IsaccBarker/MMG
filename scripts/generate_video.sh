b6f1c665c5f9acfabdde57ac28354b9f2be99b5d
IsaccBarker <milobanks@rowlandhall.org>
2022-11-29 03:02:45 -0700

    Volume adjustment

--- scripts/generate_video.sh
+++ scripts/generate_video.sh
@@ -8,7 +8,7 @@ voice_file=/tmp/${uuid}_voice.mp3
 music_file=/tmp/${uuid}_music.mp3
 music_final_file=/tmp/${uuid}_music_final.mp3
 beta_file=/tmp/${uuid}_beta.mp3
-gamma_file=/tmp/${uuid}_gamma.mp3
+gamma_file=/tmp/${uuid}_gamma.mp4
 
 # Generate voiceover.
 # BEST VOICES: BDL, CLB, FEM, SLT
@@ -21,7 +21,7 @@ ls $base/assets/background |sort -R |tail -$N |while read music_file_prime; do
 done
 
 # Generate beta audio.
-ffmpeg -i $music_file -filter:a "volume=0.25" $music_final_file
+ffmpeg -i $music_file -filter:a "volume=0.1" $music_final_file
 ffmpeg -i $voice_file -i $music_final_file -filter_complex amix=inputs=2:duration=longest $beta_file
 
 # Generate background video.
